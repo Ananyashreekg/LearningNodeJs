8e367df662339bdc79f8f223bd92edd9b207980e
Ananyashree K G <ananyashreekg933@gmail.com>
2025-06-17 14:45:18 +0530

    Executed the Asyn callback program

---
+++ callbackAsyn.js
@@ -0,0 +1,11 @@
+function loadData(callback) {
+  setTimeout(() => {
+    console.log("Data loaded from server");
+    callback(); // run after loading
+  }, 2000);
+}
+
+console.log("Loading data...");
+loadData(() => {
+  console.log("Now displaying the data");
+});
